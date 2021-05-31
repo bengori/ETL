ebdc21810614823209d801f87afed1629654818f
nataly.akhaimova <bengori22@gmail.com>
2021-05-31 19:26:33 +0500

    add ETL.py and screenshots

---
+++ ETL.py
@@ -0,0 +1,20 @@
+import psycopg2
+# извлечение данных из источника бд - 'my_database'
+conn_string= "host='localhost' port=54320 dbname='my_database' user='root' password='postgres'"
+files = ['customer', 'supplier', 'nation', 'region', 'orders', 'lineitem', 'part', 'partsupp']
+
+with psycopg2.connect(conn_string) as conn, conn.cursor() as cursor:
+    for file in files:
+        q = f"COPY {file} TO STDOUT WITH DELIMITER ',' CSV HEADER;"
+        with open(f'{file}.csv', 'w') as f:
+            cursor.copy_expert(q, f)
+
+
+conn_string2= "host='localhost' port=5433 dbname='my_database2' user='root' password='postgres'"
+with psycopg2.connect(conn_string2) as conn, conn.cursor() as cursor:
+    for file in files:
+        q = f"COPY {file} from STDIN WITH DELIMITER ',' CSV HEADER;"
+        with open(f'{file}.csv', 'r') as f:
+            cursor.copy_expert(q, f)
+        cursor.execute(f'select count(*) from {file}')
+        print(cursor.fetchall())
